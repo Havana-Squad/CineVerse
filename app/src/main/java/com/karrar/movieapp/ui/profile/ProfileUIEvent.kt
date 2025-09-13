c70c2842272fa948a4aaacab99f27ce1af5538fb
Malak Raef <malak.raef20@gmail.com>
2025-09-13 05:11:03 +0300

    feat: make the required changes in the profile viewmodel and fragment to update language, theme, login state

--- app/src/main/java/com/karrar/movieapp/ui/profile/ProfileUIEvent.kt
+++ app/src/main/java/com/karrar/movieapp/ui/profile/ProfileUIEvent.kt
@@ -3,6 +3,7 @@ package com.karrar.movieapp.ui.profile
 sealed interface ProfileUIEvent {
     object LoginEvent : ProfileUIEvent
     object RatedMoviesEvent : ProfileUIEvent
-    object DialogLogoutEvent : ProfileUIEvent
     object WatchHistoryEvent : ProfileUIEvent
+    object MyCollectionEvent : ProfileUIEvent
+    object OpenEditProfileWebsite : ProfileUIEvent
 }
