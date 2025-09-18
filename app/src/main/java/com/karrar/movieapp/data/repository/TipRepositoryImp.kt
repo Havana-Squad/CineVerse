371d28ec23d82816b55525dca93ed87034864263
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-09-18 18:48:11 +0300

    refactor: move tip data store to app preference

--- app/src/main/java/com/karrar/movieapp/data/repository/TipRepositoryImp.kt
+++ app/src/main/java/com/karrar/movieapp/data/repository/TipRepositoryImp.kt
@@ -1,16 +1,16 @@
 package com.karrar.movieapp.data.repository
 
-import com.karrar.movieapp.data.local.TipConfiguration
+import com.karrar.movieapp.data.local.AppPreferences
 import javax.inject.Inject
 
 class TipRepositoryImp @Inject constructor(
-    private val tipConfiguration: TipConfiguration,
+    private val appPreferences: AppPreferences,
 ) : TipRepository, BaseRepository()  {
     override suspend fun getCategoryTipStatus(): Boolean {
-        return tipConfiguration.getCategoryTipStatus()
+        return appPreferences.getCategoryTipStatus()
     }
 
     override suspend fun closeCategoryTip() {
-        tipConfiguration.closeCategoryTip()
+        appPreferences.closeCategoryTip()
     }
 }
--- app/src/main/java/com/karrar/movieapp/di/LocalDataSourceModule.kt
+++ app/src/main/java/com/karrar/movieapp/di/LocalDataSourceModule.kt
@@ -4,8 +4,6 @@ import com.karrar.movieapp.data.local.AppConfiguration
 import com.karrar.movieapp.data.local.AppConfigurator
 import com.karrar.movieapp.data.local.AppPreferences
 import com.karrar.movieapp.data.local.AppPreferencesImpl
-import com.karrar.movieapp.data.local.TipConfiguration
-import com.karrar.movieapp.data.local.TipConfigurator
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -23,9 +21,4 @@ abstract class LocalDataSourceModule {
     @Singleton
     @Binds
     abstract fun bindAppPreference(appPreferencesImpl: AppPreferencesImpl): AppPreferences
-
-
-    @Singleton
-    @Binds
-    abstract fun bindTipConfiguration(tipConfigurator: TipConfigurator) : TipConfiguration
 }
